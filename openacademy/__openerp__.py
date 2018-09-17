6735b7d64140c285ed3e7447092f95c1c0f7f252
Miguel Hiciano <mhiciano@atrivia.do>
2018-09-17 19:46:01 +0000

    first commit

---
+++ openacademy/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    'name': "Open Academy",
+    'version': '0.1',
+    'depends': ['base'], #esto indica si depende de algun modulo existente 
+    'author': "Miguel Hiciano",
+    'category': 'Test',
+    'description': """
+    Open Academy module for managing trainings:
+	-Training courses 
+	-training sessions
+	-attendees registration
+    """,
+    # data files always loaded at installation
+    'data': [],
+    # data files containing optionally loaded demonstration data
+    'demo': [],
+    'installable': True,
+    'active': True
+}
+
