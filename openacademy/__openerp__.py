a97c05a659d89f78d110e9e16462dde9d63074a9
Miguel Hiciano <mhiciano@atrivia.do>
2018-09-18 20:04:17 +0000

    [REF] Update: adding view

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -1,42 +1,4 @@
 # -*- coding: utf-8 -*-
-<<<<<<< HEAD
-{
-    'name': "openacademy",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        Long description of module's purpose
-    """,
-
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
-
-    # always loaded
-    'data': [
-        # 'security/ir.model.access.csv',
-        'templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
-    'installable': True,
-    'active': True
-}
-=======
-
 {
 
     'name': "Open Academy",
@@ -51,11 +13,9 @@
 	-attendees registration
     """,
     # data files always loaded at installation
-    'data': [],
+    'data': ['view/view.xml' ],
     # data files containing optionally loaded demonstration data
-    'demo': [],
+    'demo': ['demo/demo.xml' ],
     'installable': True,
     'active': True
 }
-
->>>>>>> 0ff3f62932172ce445e9d5a1b0e75817da80a75c
